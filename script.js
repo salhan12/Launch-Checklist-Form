65de1f8f5a888778bfb52a8a8efc1a31faba2e12
salhan12 <anita_salhan@comcast.com>
2022-07-29 17:16:28 -0400

    updated planets

--- script.js
+++ script.js
@@ -17,10 +17,10 @@
 
 window.addEventListener("load", function () {
 
-   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-      response.json().then(function(json){
-         const missionDestination = document.getElementById ("missionTarget");
-         const index = Math.floor(Math.random()*json.length);
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+      response.json().then(function (json) {
+         const missionDestination = document.getElementById("missionTarget");
+         const index = Math.floor(Math.random() * json.length);
          missionTarget.innerHTML = `
              <h2>Mission Destination</h2>
              <ul>
@@ -33,113 +33,120 @@ window.addEventListener("load", function () {
             <img src="${json[index].image}">
          `;
 
-     
+
       });
-     
-   // let listedPlanetsResponse = myFetch();
-   //  listedPlanetResponse.then (function(result) {
 
-   //  }
-   let form = document.querySelector("form")
-   form.addEventListener("submit", function (event) {
+      // let listedPlanetsResponse = myFetch();
+      //  listedPlanetResponse.then (function(result) {
 
-
-      //let statusCheck = document.getElementById
+      //  }
+      let form = document.querySelector("form")
+      form.addEventListener("submit", function (event) {
 
 
-      let pilotName = document.querySelector("input[name=pilotName]");
-      let copilotName = document.querySelector("input[name=copilotName]");
-      let fuelLevel = document.querySelector("input[name=fuelLevel]");
-      let cargoMass = document.querySelector("input[name=cargoMass]");
-      let pilotStatus = document.getElementById("pilotStatus");
-      let copilotStatus = document.getElementById("copilotStatus");
-      let fuelStatus = document.getElementById("fuelStatus");
-      let launchStatus  = document.getElementById("launchStatus");
-      let itemstatus = document.getElementById("itemStatus");
-      event.preventDefault();
-      // let listedPlanets;
+         //let statusCheck = document.getElementById
 
-      //TODO 1: set up an window load handler
 
-      //TODO 2:  set up a submit handler for the form
-
-      //TODO 3: Cancel submission using event.preventDefault()
-
-      //TODO 4 validate that all inputs have data
-
-      //TODO 5 check fuel level and cargo Mass, and report launch status 
-
-      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-         alert("All fields are required.");
-      
-      }
-
-      if (isNaN(pilotName.value) || isNaN(copilotName.value)) {
-         pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready`;
-         copilotStatus.innerHTML = `Copilot ${copilotName.value} is ready`;
-
-      }
-      else {
-         alert("Pilot & Co pilot names have to be letters and not numbers");
+         let pilotName = document.querySelector("input[name=pilotName]");
+         let copilotName = document.querySelector("input[name=copilotName]");
+         let fuelLevel = document.querySelector("input[name=fuelLevel]");
+         let cargoMass = document.querySelector("input[name=cargoMass]");
+         let pilotStatus = document.getElementById("pilotStatus");
+         let copilotStatus = document.getElementById("copilotStatus");
+         let fuelStatus = document.getElementById("fuelStatus");
+         let launchStatus = document.getElementById("launchStatus");
+         let itemstatus = document.getElementById("itemStatus");
          event.preventDefault();
+         // let listedPlanets;
 
-      }
+         //TODO 1: set up an window load handler
 
-      if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
-         alert("Please enter Fuel level & Cargo Mass in integer form");
-         event.preventDefault();
-      }
+         //TODO 2:  set up a submit handler for the form
 
-      else {
+         //TODO 3: Cancel submission using event.preventDefault()
+
+         //TODO 4 validate that all inputs have data
+
+         //TODO 5 check fuel level and cargo Mass, and report launch status 
          
-         itemstatus.style.visibility = "visible"
-       if (fuelLevel.value< 10000 && cargoMass.value >10000){
-         fuelStatus.innerHTML =  `Not enough Fuel for the journey`;
-         launchStatus.innerHTML = `Shuttle not ready for launch`;
-         cargoStatus.innerHTML = `Cargo mass is not low enough for launch`;
-         launchStatus.style.color = `Red`;
-       }
+         if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+            alert("All fields are required.");
+            event.preventDefault();
 
-        else if (fuelLevel.value < 10000){
-            fuelStatus.innerHTML =  `Not enough Fuel for the journey`;
-            launchStatus.innerHTML = `Shuttle not ready for launch`;
-            cargoStatus.innerHTML = `Cargo mass is low enough for launch`;
-            launchStatus.style.color = `Red`;
          }
-        
+
+         if (isNaN(pilotName.value) || isNaN(copilotName.value)) {
+            pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready`;
+            copilotStatus.innerHTML = `Copilot ${copilotName.value} is ready`;
+
+         }
+         else {
+            alert("Pilot & Co pilot names have to be letters and not numbers");
+            event.preventDefault();
+
+         }
+
+         if (isNaN(fuelLevel.value) || isNaN(cargoMass.value) || fuelLevel.value.length == 0 || cargoMass.value.length == 0) {
+            alert("Please enter Fuel level & Cargo Mass in integer form");
+            event.preventDefault();
          
-         
-         else if (cargoMass.value > 10000) {
-            launchStatus.style.color = `Red`;
-            cargoStatus.innerHTML = `Cargo mass is too much for the shuttle to take off`;
-            fuelStatus.innerHTML = `Fuel level high enough for launch`;
-            launchStatus.innerHTML = `Shuttle is not ready for launch`;
-            
-            
             
          }
-         else if (fuelLevel.value >= 10000 && cargoMass.value <= 10000) {
-           launchStatus.style.color = `Green`;
-           fuelStatus.innerHTML = `Fuel level high enough for launch`;
-           cargoStatus.innerHTML = `Cargo mass low enough for launch`;
-           launchStatus.innerHTML = `Shuttle is ready for launch`;
-      }
-         
-         
-      }
-      
-      //TODO 6 make the list visible 
-      
-      //TODO 7 fetch planet data 
-         
-         
-   
 
-   
-   
-   
-   
+         else {
 
-   });
+            itemstatus.style.visibility = "visible"
+
+            if (fuelLevel.value < 10000 && cargoMass.value > 10000) {
+               fuelStatus.innerHTML = `Not enough Fuel for the journey`;
+               launchStatus.innerHTML = `Shuttle not ready for launch`;
+               cargoStatus.innerHTML = `Cargo mass is not low enough for launch`;
+               launchStatus.style.color = `Red`;
+
+            }
+
+            else if (fuelLevel.value < 10000) {
+               fuelStatus.innerHTML = `Not enough Fuel for the journey`;
+               launchStatus.innerHTML = `Shuttle not ready for launch`;
+               cargoStatus.innerHTML = `Cargo mass is low enough for launch`;
+               launchStatus.style.color = `Red`;
+            }
+
+
+
+            else if (cargoMass.value > 10000) {
+               launchStatus.style.color = `Red`;
+               cargoStatus.innerHTML = `Cargo mass is too much for the shuttle to take off`;
+               fuelStatus.innerHTML = `Fuel level high enough for launch`;
+               launchStatus.innerHTML = `Shuttle is not ready for launch`;
+
+
+
+            }
+            else if (fuelLevel.value >= 10000 && cargoMass.value <= 10000) {
+               launchStatus.style.color = `Green`;
+               fuelStatus.innerHTML = `Fuel level high enough for launch`;
+               cargoStatus.innerHTML = `Cargo mass low enough for launch`;
+               launchStatus.innerHTML = `Shuttle is ready for launch`;
+
+
+            }
+
+        // }
+      }
+
+         //TODO 6 make the list visible 
+
+         //TODO 7 fetch planet data 
+
+
+
+
+
+
+
+
+
+      });
    });
 });
